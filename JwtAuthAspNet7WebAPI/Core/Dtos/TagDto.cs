76b1eec28db67de81616e4ea9d2cf99362191682
tmahir99 <mahirtahirovic@hotmail.com>
2025-01-19 00:02:41 +0100

    Update app to add TagDto

---
+++ JwtAuthAspNet7WebAPI/Core/Dtos/TagDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNet7WebAPI.Core.Dtos
+{
+    public class TagDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+    }
+}
---
+++ JwtAuthAspNet7WebAPI/Core/Interfaces/ITagService.cs
@@ -0,0 +1,14 @@
+﻿using JwtAuthAspNet7WebAPI.Core.Dtos;
+using JwtAuthAspNet7WebAPI.Core.Entities;
+
+namespace JwtAuthAspNet7WebAPI.Core.Interfaces
+{
+    public interface ITagService
+    {
+        Task<TagDto> CreateAsync(TagDto dto);
+        Task<TagDto> GetByIdAsync(long id);
+        Task<TagDto> GetAllAsync(TagDto filter);
+        Task UpdateAsync(long id, TagDto dto);
+        Task DeleteAsync(long id);
+    }
+}
